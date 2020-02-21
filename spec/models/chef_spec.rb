e09d93485fbee19345216b0b43b827566f8745e2
DeMarcus Kirby <tkd024@gmail.com>
2020-02-21 10:32:52 -0700

    add method test for gathering unique ingredients

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+  describe "methods" do
+    xit "should return a unique array of ingredients" do
+      chef = Chef.create!(name: "Tron")
+      dish = chef.dishes.create!(name: "Nachos", description: "Cheesey chips of the Gods!")
+      ingred1 = dish.ingredients.create!(name: "Cheese", calories: 250)
+      ingred2 = dish.ingredients.create!(name: "Chips", calories: 100)
+      ingred3 = dish.ingredients.create!(name: "Ground Beef", calories: 150)
+      dish2 = chef.dishes.create!(name: "Tacos", description: "Cheesey chips of the Gods!")
+      ingred4 = dish2.ingredients.create!(name: "Tortilla", calories: 50)
+      dish2.ingredients << ingred3
+
+      expect(chef.ingredients.length).to eq(4)
+    end
+  end
 end
