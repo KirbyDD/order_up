8bd8dac943394b5c04ba8f844b6df00270ad541e
DeMarcus Kirby <tkd024@gmail.com>
2020-02-21 10:34:55 -0700

    add ingredient method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,9 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+
+  def ingredients
+    search = dishes.joins(:dish_ingredients).select(dish_ingredient[:ingredient_id])
+    Ingredient.find(search)
+  end
 end
