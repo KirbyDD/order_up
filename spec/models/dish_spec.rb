9fb5e7e17ef7be7661e5950f71249ae18d61a39e
DeMarcus Kirby <tkd024@gmail.com>
2020-02-21 09:43:58 -0700

    Add test for total_calories method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+  describe 'methods' do
+    it 'can calculate total_calories' do
+      chef = Chef.create!(name: "Tron")
+      dish = chef.dishes.create!(name: "Nachos", description: "Cheesey chips of the Gods!")
+      ingred1 = dish.ingredients.create!(name: "Cheese", calories: 250)
+      ingred2 = dish.ingredients.create!(name: "Chips", calories: 100)
+      ingred3 = dish.ingredients.create!(name: "Ground Beef", calories: 150)
+
+      expect(dish.total_calories).to eq(500)
+    end
+  end
 end
